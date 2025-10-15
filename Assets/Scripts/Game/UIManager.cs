17129e31d251c6fc3c65f640f96b01afc0d8a9e4
thang-nt25 <nt9901116@gmail.com>
2025-10-15 18:51:35 +0700

    Update UIManager.cs

--- Assets/Scripts/Game/UIManager.cs
+++ Assets/Scripts/Game/UIManager.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityChess;
@@ -337,109 +337,18 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
         }
     }
 
-    private void OnGameResetToHalfMove() {
-		UpdateGameStringInputField();
-		moveUITimeline.HeadIndex = GameManager.Instance.LatestHalfMoveIndex / 2;
-		ValidateIndicators();
-	}
-
-	public void SetActivePromotionUI(bool value) => promotionUI.gameObject.SetActive(value);
-
-	public void OnElectionButton(int choice) => GameManager.Instance.ElectPiece((ElectedPiece)choice);
-
-	public void ResetGameToFirstHalfMove() => GameManager.Instance.ResetGameToHalfMoveIndex(0);
-
-	public void ResetGameToPreviousHalfMove() => GameManager.Instance.ResetGameToHalfMoveIndex(Math.Max(0, GameManager.Instance.LatestHalfMoveIndex - 1));
-
-	public void ResetGameToNextHalfMove() => GameManager.Instance.ResetGameToHalfMoveIndex(Math.Min(GameManager.Instance.LatestHalfMoveIndex + 1, GameManager.Instance.HalfMoveTimeline.Count - 1));
-
-	public void ResetGameToLastHalfMove() => GameManager.Instance.ResetGameToHalfMoveIndex(GameManager.Instance.HalfMoveTimeline.Count - 1);
-
-	public void StartNewGame() => GameManager.Instance.StartNewGame();
-	
-	public void LoadGame() => GameManager.Instance.LoadGame(GameStringInputField.text);
-
-	private void AddMoveToHistory(HalfMove latestHalfMove, Side latestTurnSide) {
-		RemoveAlternateHistory();
-		
-		switch (latestTurnSide) {
-			case Side.Black: {
-				if (moveUITimeline.HeadIndex == -1) {
-					FullMoveUI newFullMoveUI = Instantiate(moveUIPrefab, moveHistoryContentParent.transform);
-					moveUITimeline.AddNext(newFullMoveUI);
-					
-					newFullMoveUI.transform.SetSiblingIndex(GameManager.Instance.FullMoveNumber - 1);
-					newFullMoveUI.backgroundImage.color = backgroundColor;
-					newFullMoveUI.whiteMoveButtonImage.color = buttonColor;
-					newFullMoveUI.blackMoveButtonImage.color = buttonColor;
-					
-					if (newFullMoveUI.FullMoveNumber % 2 == 0) {
-						newFullMoveUI.SetAlternateColor(moveHistoryAlternateColorDarkenAmount);
-					}
-
-					newFullMoveUI.MoveNumberText.text = $"{newFullMoveUI.FullMoveNumber}.";
-					newFullMoveUI.WhiteMoveButton.enabled = false;
-				}
-				
-				moveUITimeline.TryGetCurrent(out FullMoveUI latestFullMoveUI);
-				latestFullMoveUI.BlackMoveText.text = latestHalfMove.ToAlgebraicNotation();
-				latestFullMoveUI.BlackMoveButton.enabled = true;
-				
-				break;
-			}
-			case Side.White: {
-				FullMoveUI newFullMoveUI = Instantiate(moveUIPrefab, moveHistoryContentParent.transform);
-				newFullMoveUI.transform.SetSiblingIndex(GameManager.Instance.FullMoveNumber - 1);
-				newFullMoveUI.backgroundImage.color = backgroundColor;
-				newFullMoveUI.whiteMoveButtonImage.color = buttonColor;
-				newFullMoveUI.blackMoveButtonImage.color = buttonColor;
-
-				if (newFullMoveUI.FullMoveNumber % 2 == 0) {
-					newFullMoveUI.SetAlternateColor(moveHistoryAlternateColorDarkenAmount);
-				}
-
-				newFullMoveUI.MoveNumberText.text = $"{newFullMoveUI.FullMoveNumber}.";
-				newFullMoveUI.WhiteMoveText.text = latestHalfMove.ToAlgebraicNotation();
-				newFullMoveUI.BlackMoveText.text = "";
-				newFullMoveUI.BlackMoveButton.enabled = false;
-				newFullMoveUI.WhiteMoveButton.enabled = true;
-				
-				moveUITimeline.AddNext(newFullMoveUI);
-				break;
-			}
-		}
-
-		moveHistoryScrollbar.value = 0;
-	}
-
-	private void RemoveAlternateHistory() {
-		if (!moveUITimeline.IsUpToDate) {
-			GameManager.Instance.HalfMoveTimeline.TryGetCurrent(out HalfMove lastHalfMove);
-            if (resultText != null)
-                resultText.gameObject.SetActive(lastHalfMove.CausedCheckmate);
-			List<FullMoveUI> divergentFullMoveUIs = moveUITimeline.PopFuture();
-			foreach (FullMoveUI divergentFullMoveUI in divergentFullMoveUIs) {
-				Destroy(divergentFullMoveUI.gameObject);
-			}
-		}
-	}
-
-	private void ValidateIndicators() {
-		Side sideToMove = GameManager.Instance.SideToMove;
-		whiteTurnIndicator.enabled = sideToMove == Side.White;
-		blackTurnIndicator.enabled = sideToMove == Side.Black;
-	}
-
-	private void UpdateGameStringInputField() {
-        if (GameStringInputField != null) {
+    private void UpdateGameStringInputField()
+    {
+        if (GameStringInputField != null)
+        {
             GameStringInputField.text = GameManager.Instance.SerializeGame();
         }
     }
 
-	public void GoToMainMenu()
-	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-	}
+    public void GoToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
 
 
     public void OnPauseButtonClicked()
