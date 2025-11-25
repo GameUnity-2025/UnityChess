9907a2b41c2f2ada34cf1762ea0d1cd17cdabb9d
Tho0205 <tho02052004@gmail.com>
2025-11-25 09:29:26 +0700

    Feature: Add new Mute Button on MainMenu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,29 +4,58 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Audio Settings")]
     public AudioMixer audioMixer;
     public Slider musicSlider;
     public Slider sfxSlider;
 
-    private const float minVolume = 0.0001f; 
-    private const float muteVolume = -80f;  
+    [Header("Mute Button Settings")]
+    public Image muteButtonImage;   
+    public Sprite soundOnIcon;      
+    public Sprite soundOffIcon;     
+
+    private const float minVolume = 0.0001f;
+    private const float muteVolume = -80f;
+    private bool isMuted = false;
 
     void Start()
     {
-        PlayerPrefs.DeleteKey("MusicVolume");
-        PlayerPrefs.DeleteKey("SFXVolume");
-
         float musicVol = PlayerPrefs.GetFloat("MusicVolume", 1f);
         float sfxVol = PlayerPrefs.GetFloat("SFXVolume", 1f);
 
         musicSlider.value = musicVol;
         sfxSlider.value = sfxVol;
 
+        isMuted = PlayerPrefs.GetInt("IsMuted", 0) == 1;
+
         ApplyMusicVolume(musicVol);
         ApplySFXVolume(sfxVol);
+        UpdateMuteState(); 
 
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+        UpdateMuteState();
+
+        PlayerPrefs.SetInt("IsMuted", isMuted ? 1 : 0);
+    }
+
+    private void UpdateMuteState()
+    {
+        if (isMuted)
+        {
+            AudioListener.volume = 0; 
+            if (muteButtonImage != null) muteButtonImage.sprite = soundOffIcon;
+        }
+        else
+        {
+            AudioListener.volume = 1; 
+            if (muteButtonImage != null) muteButtonImage.sprite = soundOnIcon;
+        }
     }
 
     public void SetMusicVolume(float volume)
@@ -43,13 +72,13 @@ public class AudioManager : MonoBehaviour
 
     private void ApplyMusicVolume(float volume)
     {
-        float dB = (volume <= 0.0001f) ? muteVolume : Mathf.Log10(volume) * 20;
+        float dB = (volume <= minVolume) ? muteVolume : Mathf.Log10(volume) * 20;
         audioMixer.SetFloat("MusicVolume", dB);
     }
 
     private void ApplySFXVolume(float volume)
     {
-        float dB = (volume <= 0.0001f) ? muteVolume : Mathf.Log10(volume) * 20;
+        float dB = (volume <= minVolume) ? muteVolume : Mathf.Log10(volume) * 20;
         audioMixer.SetFloat("SFXVolume", dB);
     }
 }
